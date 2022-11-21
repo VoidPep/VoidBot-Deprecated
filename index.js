679db0fcf367dc757267505f78782188740586f5
Void Pep <pedrohm1009@gmail.com>
2022-11-20 23:44:12 -0300

    Discord JS

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const dotenv = require('dotenv');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+require('dotenv/config');
 
-dotenv.config();
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
