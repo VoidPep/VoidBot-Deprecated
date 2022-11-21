00556b07f8127a24d3a67e16bdeff7509930022a
Void Pep <pedrohm1009@gmail.com>
2022-11-21 14:34:55 -0300

    discord bot alpha 0.1

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+/* eslint-disable brace-style */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable space-before-blocks */
+/* eslint-disable no-empty-function */
+
+const { Client, Events, GatewayIntentBits, Collection, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
 require('dotenv/config');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// dotenv configs
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	] });
 client.commands = new Collection();
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
+client.on('messageCreate', (message) => {
+	console.log(message.content);
+});
+client.on('messageDelete', (message) => {
+	console.log(`Uma mensagem foi apagada por ${message.author.tag}\nconteúdo da mensagem:\n${message.content}`);
+});
 
-client.login(process.env.TOKEN);
+async function main() {
+	const commands = [
+		{
+			name: 'ping',
+			description: 'replies with pong',
+		},
+		{
+			name: 'uwu',
+			description: 'teste',
+		},
+	];
+	
+	try {
+		console.log('\nstarted refreshing aplication slash commands\n');
+		await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+			body: commands,
+		});
+		client.login(TOKEN);
+	} catch (err){
+		console.log(err);
+	}
+}
+main();
