16271264788edc58046c60173f4371aa2c2ec283
Void Pep <pedrohm1009@gmail.com>
2023-02-03 22:56:50 -0300

    Remoção do subcommand music

--- commands/play.js
+++ commands/play.js
@@ -6,13 +6,6 @@ module.exports = {
         .setName('play')
         .setDescription('Toca músicas diretamente do youtube')
         .addSubcommand((subcommand) =>
-            subcommand.setName("music")
-                .setDescription('Toca uma única singular música')
-                .addStringOption((option) =>
-                    option.setName('url')
-                        .setDescription('url da música')
-                        .setRequired(true)))
-        .addSubcommand((subcommand) =>
             subcommand.setName('playlist')
                 .setDescription('Toca a sua... playlist')
                 .addStringOption((option) =>
@@ -23,8 +16,8 @@ module.exports = {
             subcommand.setName('search')
                 .setDescription('Pesquisa pelo nome da música')
                 .addStringOption((option) =>
-                    option.setName('name')
-                        .setDescription('nome da música')
+                    option.setName('url')
+                        .setDescription('nome/url da música')
                         .setRequired(true))),
     run: async ({ client, interaction }) => {
 
@@ -42,24 +35,7 @@ module.exports = {
         const queue = await client.player.createQueue(interaction.guild);
         if (!queue.connection) await queue.connect(interaction.member.voice.channel);
 
-        if (interaction.options.getSubcommand() === "music") {
-            const url = interaction.options.getString("url");
-            const result = await client.player.search(url, {
-                requestedBy: interaction.user,
-                searchEngine: QueryType.YOUTUBE_VIDEO,
-            });
-            if (result.length === 0)
-                return interaction.editReply("❌ Sem resultados :/");
-
-            const song = result.tracks[0];
-            await queue.addTrack(song);
-            embed
-                .setDescription(`**[${song.title}](${song.url})**\n Foi adicionado à lista de reprodução`)
-                .setThumbnail(song.thumbnail)
-                .setFooter({ text: `Duração: ${song.duration}` });
-
-
-        } else if (interaction.options.getSubcommand() === 'playlist') {
+        if (interaction.options.getSubcommand() === 'playlist') {
             const url = interaction.options.getString("url");
             const result = await client.player.search(url, {
                 requestedBy: interaction.user,
