7bf445afd3554ca36d6b4874dc15c60d9b7f1b19
Void Pep <pedrohm1009@gmail.com>
2023-04-14 22:03:59 -0300

    fix

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -7,7 +7,6 @@ module.exports = {
 		if (!interaction.isChatInputCommand()) return;
 
 		const command = interaction.client.commands.get(interaction.commandName);
-		//console.log(command);
 
 		if (!command) {
 			console.error(`No command matching ${interaction.commandName} was found.`);
